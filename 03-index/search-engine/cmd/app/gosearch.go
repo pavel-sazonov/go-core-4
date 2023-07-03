25c7eb06c336986512e4e1780404dcb605cabab3
Pavel Sazonov <pavel.sazonov@lamoda.ru>
2023-07-03 23:34:08 +0300

    parse to Document

--- 03-index/search-engine/cmd/app/gosearch.go
+++ 03-index/search-engine/cmd/app/gosearch.go
@@ -14,50 +14,52 @@ const (
 
 var s = flag.String("s", "", "search argument")
 
+// Document - документ, веб-страница, полученная поисковым роботом.
+type Document struct {
+	ID    int
+	URL   string
+	Title string
+}
+
 func main() {
 	flag.Parse()
-	urls := scanResult([]string{godev, practicalgo})
+	documents := scanResult([]string{godev, practicalgo})
 
-	for _, v := range search(*s, urls) {
+	for _, v := range search(*s, documents) {
 		fmt.Println(v)
 	}
 }
 
-// поиск строки в слайсе урлов
-func search(s string, urls []string) []string {
-	var res []string
-
-	for _, url := range urls {
-		if strings.Contains(url, s) {
-			res = append(res, url)
+// поиск строки в слайсе Document
+func search(s string, documents []Document) (result []string) {
+	for _, doc := range documents {
+		if strings.Contains(doc.URL, s) {
+			result = append(result, doc.URL)
 		}
 	}
 
-	return res
+	return result
 }
 
-func scanResult(urls []string) []string {
-	var data []string
-
+func scanResult(urls []string) (documents []Document) {
 	for _, url := range urls {
-		urls, err := scan(url)
+		docs, err := scan(url)
 		if err != nil {
 			continue
 		}
-		data = append(data, urls...)
+		documents = append(documents, docs...)
 	}
-	return data
+	return documents
 }
 
-func scan(url string) (urls []string, err error) {
+func scan(url string) (documents []Document, err error) {
 	s := spider.New()
-	var res []string
 	docs, err := s.Scan(url, 2)
 	if err != nil {
 		return nil, err
 	}
 	for _, doc := range docs {
-		res = append(res, doc.URL)
+		documents = append(documents, Document{doc.ID, doc.URL, doc.Title})
 	}
-	return res, nil
+	return documents, nil
 }
