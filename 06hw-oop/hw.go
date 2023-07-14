ce82aa3c5dedfeb5e6d67c254debb6be1cb0225c
Pavel Sazonov <pavel.sazonov@lamoda.ru>
2023-07-14 10:37:53 +0300

    add comments

--- 06hw-oop/hw.go
+++ 06hw-oop/hw.go
@@ -5,21 +5,20 @@ import (
 	"math"
 )
 
-// По условиям задачи, координаты не могут быть меньше 0.
-
+// Geom представляет две точки.
 type Geom struct {
 	X1, Y1, X2, Y2 float64
 }
 
+// CalculateDistance рассчитывает расстояние между двумя точками
 // По условиям задачи, координаты не могут быть меньше 0.
 func (geom Geom) CalculateDistance() (distance float64, err error) {
-	// добавил возврат ошибки и проверил в тесте
 	if geom.X1 < 0 || geom.X2 < 0 || geom.Y1 < 0 || geom.Y2 < 0 {
+		// добавил возврат ошибки и проверил в тесте
 		return -1, fmt.Errorf("координаты не могут быть меньше нуля")
 	}
 	// убрал else
 
-	// возврат расстояния между точками
 	distance = math.Sqrt(math.Pow(geom.X2-geom.X1, 2) + math.Pow(geom.Y2-geom.Y1, 2))
 	return distance, nil
 
