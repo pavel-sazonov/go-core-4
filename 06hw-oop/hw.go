b243050c4ccb75a34b7f6543e514c3fd34f8a4e4
Pavel Sazonov <pavel.sazonov@lamoda.ru>
2023-07-14 09:33:10 +0300

    add err and remove else

--- 06hw-oop/hw.go
+++ 06hw-oop/hw.go
@@ -1,4 +1,4 @@
-package hw
+package geometry // поменял название
 
 import (
 	"fmt"
@@ -11,15 +11,16 @@ type Geom struct {
 	X1, Y1, X2, Y2 float64
 }
 
-func (geom Geom) CalculateDistance() (distance float64) {
-
+// По условиям задачи, координаты не могут быть меньше 0.
+func (geom Geom) CalculateDistance() (distance float64, err error) {
+	// добавил возврат ошибки и проверил в тесте
 	if geom.X1 < 0 || geom.X2 < 0 || geom.Y1 < 0 || geom.Y2 < 0 {
-		fmt.Println("Координаты не могут быть меньше нуля")
-		return -1
-	} else {
-		distance = math.Sqrt(math.Pow(geom.X2-geom.X1, 2) + math.Pow(geom.Y2-geom.Y1, 2))
+		return -1, fmt.Errorf("координаты не могут быть меньше нуля")
 	}
+	// убрал else
 
 	// возврат расстояния между точками
-	return distance
+	distance = math.Sqrt(math.Pow(geom.X2-geom.X1, 2) + math.Pow(geom.Y2-geom.Y1, 2))
+	return distance, nil
+
 }
--- 06hw-oop/hw_test.go
+++ 06hw-oop/hw_test.go
@@ -1,4 +1,4 @@
-package hw
+package geometry
 
 import "testing"
 
@@ -10,13 +10,21 @@ func TestGeom_CalculateDistance(t *testing.T) {
 	}{
 		{
 			name:         "#1",
+			geom:         Geom{X1: -1, Y1: 1, X2: 4, Y2: 5},
+			wantDistance: -1,
+		},
+		{
+			name:         "#2",
 			geom:         Geom{X1: 1, Y1: 1, X2: 4, Y2: 5},
 			wantDistance: 5,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if gotDistance := tt.geom.CalculateDistance(); gotDistance != tt.wantDistance {
+			if _, err := tt.geom.CalculateDistance(); err != nil {
+				t.Log(err)
+			}
+			if gotDistance, _ := tt.geom.CalculateDistance(); gotDistance != tt.wantDistance {
 				t.Errorf("Geom.CalculateDistance() = %v, want %v", gotDistance, tt.wantDistance)
 			}
 		})
