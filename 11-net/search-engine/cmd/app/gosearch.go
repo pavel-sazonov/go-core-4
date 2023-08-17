d194a3287686e0c1aac691d8b592b98195a5578c
Pavel Sazonov <pavel.sazonov@lamoda.ru>
2023-08-17 13:23:15 +0300

    refactor main

--- 11-net/search-engine/cmd/app/gosearch.go
+++ 11-net/search-engine/cmd/app/gosearch.go
@@ -21,34 +21,23 @@ const (
 func main() {
 	searchString := "Go"
 	var documents []index.Document
+	readOK := false
 
-	f, err := os.Open(docsFile)
-	if err != nil {
-		log.Println(err)
+	if _, err := os.Stat(docsFile); err == nil {
+		documents, err = readFromFile()
+		if err == nil {
+			readOK = true
+		} else {
+			log.Println(err)
+		}
+	}
+
+	if !readOK {
 		documents = scan([]string{godev, practicalgo})
 		sort.SliceStable(documents, func(i, j int) bool {
 			return documents[i].ID < documents[j].ID
 		})
-
-		f, err = os.Create(docsFile)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		defer f.Close()
-
-		err = store(documents, f)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-	} else {
-		documents, err = get(f)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		log.Println(len(documents))
+		saveToFile(documents)
 	}
 
 	index := index.Make(documents)
@@ -61,6 +50,20 @@ func main() {
 			fmt.Println(documents[i])
 		}
 	}
+}
+
+func readFromFile() (documents []index.Document, err error) {
+	f, err := os.Open(docsFile)
+	if err != nil {
+		return nil, err
+	}
+
+	documents, err = get(f)
+	if err != nil {
+		return nil, err
+	}
+
+	return documents, nil
 }
 
 func scan(urls []string) (data []index.Document) {
@@ -84,13 +87,25 @@ func scan(urls []string) (data []index.Document) {
 	return data
 }
 
-func store(docs []index.Document, w io.Writer) error {
+func saveToFile(docs []index.Document) {
+	f, err := os.Create(docsFile)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer f.Close()
+
 	b, err := json.Marshal(docs)
-	if err == nil {
-		_, err = w.Write(b)
+	if err != nil {
+		log.Println(err)
+		return
 	}
 
-	return err
+	_, err = f.Write(b)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
 
 func get(r io.Reader) (docs []index.Document, err error) {
